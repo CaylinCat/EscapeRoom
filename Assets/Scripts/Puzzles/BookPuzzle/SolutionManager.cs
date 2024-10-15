e4a127cbdeac24936e4bec8da629b2d506fe94e8
CaylinCat <60459726+CaylinCat@users.noreply.github.com>
2024-10-14 17:40:53 -0700

    added solution

--- Assets/Scripts/Puzzles/BookPuzzle/SolutionManager.cs
+++ Assets/Scripts/Puzzles/BookPuzzle/SolutionManager.cs
@@ -1,14 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SolutionManager : MonoBehaviour
 {
-    public Transform[] correctOrder;
+    public Transform[] bookSlots;
+    public GameObject[] correctOrder;
 
     public void CheckPuzzleSolved()
     {
-        for (int i = 0; i < correctOrder.Length; i++)
+        for (int i = 0; i < bookSlots.Length; i++)
         {
-            if (correctOrder[i].childCount == 0 || correctOrder[i].GetChild(0).gameObject != correctOrder[i].gameObject)
+            if (bookSlots[i].childCount == 0)
+            {
+                Debug.Log("Puzzle is not solved yet.");
+                return;
+            }
+
+            GameObject bookInSlot = bookSlots[i].GetChild(0).gameObject;
+            if (bookInSlot != correctOrder[i])
             {
                 Debug.Log("Puzzle is not solved yet.");
                 return;
