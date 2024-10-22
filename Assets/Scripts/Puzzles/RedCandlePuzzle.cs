772a60f6c7c7c84f73b6bbfcdf52c5958fa6af71
grace-ortiz <gortiz@ucsd.edu>
2024-10-21 22:17:39 -0700

    red candle partial fix

--- Assets/Scripts/Puzzles/RedCandlePuzzle.cs
+++ Assets/Scripts/Puzzles/RedCandlePuzzle.cs
@@ -11,27 +11,43 @@ public class RedCandlePuzzle : Puzzle
     public GameObject WaxImg;
     public FMODUnity.StudioEventEmitter IgniteSFX;
     private bool complete = false;
+    private bool candleLit = false; 
+    private bool waxCollected = false;
 
     public void GrabWax()
     {
-        if(complete) return;
+        if (complete || waxCollected) return;
 
         InventoryManager.Instance.AddItem(Wax);
         WaxImg.SetActive(false);
+        waxCollected = true;
         complete = true;
+        Debug.Log("Wax collected");
         OnComplete();
     }
 
     public void LightCandle()
     {
+        if (candleLit) return;
+
         IgniteSFX.Play();
         spriteRenderer.sprite = litCandleSprite;
         WaxImg.SetActive(true);
-        Debug.Log("triggered light candle");
+        Debug.Log("Candle lit");
+        candleLit = true;
     }
 
     public void Start()
     {
-        WaxImg.SetActive(false);
+       WaxImg.SetActive(candleLit && !waxCollected);
+       Debug.Log("Wax collected: " + waxCollected);
+       Debug.Log("Candle lit: " + candleLit);
+    }
+
+    public void OnEnable()
+    {
+        WaxImg.SetActive(candleLit && !waxCollected);
+        Debug.Log("enable Wax collected: " + waxCollected);
+        Debug.Log("enable Candle lit: " + candleLit);
     }
 }
